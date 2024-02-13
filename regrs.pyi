b18ae3d8b17049283f48e22687e572ade2ea0b2b
FatihDmrcn <fatihdmrcn@outlook.com>
2024-02-13 23:44:34 +0100

    removed static function

--- regrs.pyi
+++ regrs.pyi
@@ -1,14 +1,5 @@
 import numpy as np
 
-def r2_predicted(exog, endog) -> float:
-    """
-    Computing the predicted R² by iteratively
-    leaving one row from, exog. and endog. data
-    and computing the error based on the left out
-    data row. 
-
-    :return: Predicted R² as float
-    """
 
 class OLS:
     def __init__(self, exog: np.ndarray, endog: np.ndarray):
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 use rayon::prelude::*;
-use numpy::{ndarray::*,PyReadonlyArray1,PyReadonlyArray2};
+use numpy::{PyReadonlyArray1, PyReadonlyArray2};
+use numpy::ndarray::*;
 use ndarray_linalg::LeastSquaresSvd;
 
 
@@ -17,14 +18,15 @@ struct OLS{
 impl OLS{
     #[new]
     fn new(exog: PyReadonlyArray2<f64>, endog: PyReadonlyArray1<f64>) -> Self {
-        let exog = exog.to_owned_array();
-        let endog = endog.to_owned_array();
-        Self{exog, endog}
+        let _exog: Array2<f64> = exog.to_owned_array();
+        let _endog: Array1<f64> = endog.to_owned_array();
+        Self{
+            exog: _exog, 
+            endog: _endog, 
+        }
     }
 
     fn r2_predicted(&self) -> PyResult<f64> {
-        // let exog = Self::exog.to_owned_array();
-        // let endog = Self::endog.to_owned_array();
         let len = self.exog.nrows();
         let mut errors: Vec<f64> = Vec::new();
         (0..len).into_par_iter()
@@ -40,39 +42,12 @@ impl OLS{
             })
             .collect_into_vec(&mut errors);
         let press = Array1::from_vec(errors).sum();
-        let r2_predicted = 1. - (press / self.endog.var(0.)) / len as f64;
-        Ok(r2_predicted)
+        Ok(1. - (press / self.endog.var(0.)) / len as f64)
     }
 }
-
-
-#[pyfunction]
-fn r2_predicted(exog: PyReadonlyArray2<f64>, endog: PyReadonlyArray1<f64>) -> PyResult<f64> {
-    let exog = exog.to_owned_array();
-    let endog = endog.to_owned_array();
-    let len = exog.nrows();
-    let mut errors: Vec<f64> = Vec::new();
-    (0..len).into_par_iter()
-        .map(|_i| {
-            let mut a_train = exog.clone();
-            a_train.remove_index(Axis(0), _i);
-            let mut b_train = endog.clone();
-            b_train.remove_index(Axis(0), _i);
-            let fit = a_train.least_squares(&b_train).unwrap().solution;
-            let a_test = exog.row(_i);
-            let b_test = endog.get(_i);
-            (b_test.unwrap() - fit.dot(&a_test)).powf(2.0)
-        })
-        .collect_into_vec(&mut errors);
-    let press = Array1::from_vec(errors).sum();
-    let r2_predicted = 1. - (press / endog.var(0.)) / len as f64;
-    Ok(r2_predicted)
-}
-
 
 #[pymodule]
 fn regrs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<OLS>()?;
-    m.add_function(wrap_pyfunction!(r2_predicted, m)?)?;
     Ok(())
 }
